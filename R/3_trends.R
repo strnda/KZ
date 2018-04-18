a128ece373443a0c2bdcb30eca7300096b04b903
Filip Strnad <strnadf@fzp.czu.cz>
2018-04-18 14:39:23 +0200

    Update 3_trends.R

--- R/3_trends.R
+++ R/3_trends.R
@@ -27,9 +27,10 @@ ggplot(data = dta.melt, aes(x = DTM, y = value, group = variable, color = variab
 ggplot(data = dta.melt, aes(x = DTM, y = value, group = variable, color = variable)) +
   scale_color_manual(values = c('royalblue', 'orange', 'steelblue', 'firebrick4', 'firebrick1', 'firebrick')) +
   geom_line() +
-  stat_smooth(method = 'lm', se = FALSE , color = 'limegreen') +
-  stat_smooth(method = 'lm', se = FALSE , formula = y ~ x + I(x^2), color = 'darkgreen') +
+  stat_smooth(method = 'lm', se = FALSE , formula = y ~ x, color = 'green4') +
+  stat_poly_eq(aes(label = paste(..rr.label..)), label.x.npc = 'right', formula = y ~ x, parse = TRUE, size = 3, colour = 'black') +
+  stat_fit_glance(method = 'lm', method.args = list(formula = y ~ x), geom = 'text', aes(label = paste('P-value = ', signif(..p.value.., digits = 4), sep = '')), label.x.npc = 'left', size = 3, colour = 'black') +
   facet_wrap(~variable, scales = 'free') +
   theme_bw() +
   theme(legend.position = 'none') +
-  ggtitle('Linearni a kvadraticky trend')
+  ggtitle('Linearni trend')
