c567b294462d9f755571dfe4d159285d8fdfa17d
Filip Strnad <strnadf@fzp.czu.cz>
2021-04-15 11:15:55 +0200

    Update 1_data_import.R

--- R/1_data_import.R
+++ R/1_data_import.R
@@ -4,17 +4,18 @@ to.instal <- lop[which(!lop %in% installed.packages()[,'Package'])]
 
 if(length(to.instal) != 0) install.packages(to.instal)
 
-lapply(lop, library, character.only = T)
+temp <- lapply(lop, library, character.only = T)
+rm(temp)
 
-id <- '01048000'
-year <- 1945
 
-dta <- as.data.table(read.fwf(sprintf('ftp://nero.hwr.arizona.edu/mopex/MOPEX_daily/%s.dly', id), 
-                              widths = c(8, 10, 10, 10, 10, 10)))
-names(dta) <- c('DTM', 'P', 'E', 'R', 'Tmax', 'Tmin')
-dta <- dta[, `:=`(DTM = as.Date(gsub(' ','0', DTM), format = '%Y%m%d'), 
-                  T = rowMeans(.SD)),
-           .SDcols = c('Tmax', 'Tmin')]
 
-dta <- dta[R >= 0,]
-dta <- dta[DTM %between% c(as.Date(paste(year, '11-1', sep = '-')), as.Date(paste(year + 20, '10-31', sep = '-')))]
+## data dostupna z https://owncloud.cesnet.cz/index.php/s/zMUkK2EYnrKlmtW
+
+dta <- fread(input = "../../../ownCloud/Active Docs/vyuka/klima/data skola/4215-skola.dat",
+             col.names = c("rem1", "P", "R", "T", "H", "rem2"))
+dta[, `:=`(rem1 = NULL, 
+           rem2 = NULL,
+           DTM = seq(from = as.Date(x = "1962-11-1", 
+                                    format = "%Y-%m-%d"),
+                     by = "month",
+                     length.out = .N))]
